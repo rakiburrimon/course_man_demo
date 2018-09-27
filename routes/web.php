1359ca0763e6346ec1345a06d0e025ceb10e5663
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2018-09-27 15:33:29 +0600

    9

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register','RegisterController@Register');
+Route::get('/register','RegisterController@register');
 
 Auth::routes();
 

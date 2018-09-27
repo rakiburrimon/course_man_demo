e2aabe1a01a92eeb443977c64ae864549ad7ee6f
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2018-09-27 12:23:08 +0600

    second

--- database/migrations/employee_table.php
+++ database/migrations/employee_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateEmployeeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('admin', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('password');
+        Schema::create('employee', function (Blueprint $table) {
+            $table->increments('employee_id');
+            $table->string('employee_name');
+            $table->string('employee_password');
+
             $table->rememberToken();
             $table->timestamps();
         });
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('employee');
     }
 }
